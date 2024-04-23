45ca82e3f45e056e7b7ec5b7184b9f9d6dd0d538
NandanXDm <nanven9@gmail.com>
2024-04-23 19:54:00 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/search', async(req, res) => {
+   
+    fetch('https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=Nandan').then(response=>{
+        return response.json();
+    }).then(response=>{
+        res.send(response);
+    })
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
